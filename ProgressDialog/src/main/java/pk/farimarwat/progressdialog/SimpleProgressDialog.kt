4ebb80cc52b41d9bd04e37c270a50f3ecef3d385
Farman Ullah Khan <farimarwat@gmail.com>
2022-11-28 15:12:46 +0500

    initial commit

---
+++ ProgressDialog/src/main/java/pk/farimarwat/progressdialog/SimpleProgressDialog.kt
@@ -0,0 +1,91 @@
+package pk.farimarwat.progressdialog
+
+import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.content.Context
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
+
+@SuppressLint("InflateParams")
+class SimpleProgressDialog private constructor(builder: Builder){
+    private var mDialog:AlertDialog? = null
+    private var mMsg:String? = null
+    private var mBackground:Drawable? = null
+    private var mProgresssColor = 0
+    private var mTextColor = 0
+    class Builder(var context: Context){
+        private var mMsg:String? = null
+        private var mBackground:Drawable? = null
+        private var mProgresssColor = 0
+        private var mTextColor = 0
+
+        fun setMessage(msg:String) = apply { this.mMsg = msg }
+        fun getMessage() = this.mMsg
+
+        fun setBackgroundDrawable(background:Drawable?) = apply { this.mBackground = background }
+        fun getBackgroundDrawable() = this.mBackground
+
+        //Setting Progress Color
+        fun setProgressColor(color:Int) = apply { this.mProgresssColor = color }
+        fun getProgressColor() = this.mProgresssColor
+
+        //Setting Text Color
+        fun setTextColor(color:Int) = apply { this.mTextColor = color }
+        fun getTextColor() = this.mTextColor
+
+        fun build() = SimpleProgressDialog(this)
+    }
+    init {
+        this.mMsg = builder.getMessage()
+        this.mBackground = builder.getBackgroundDrawable()
+        this.mProgresssColor = builder.getProgressColor()
+        this.mTextColor = builder.getTextColor()
+
+
+        val view = LayoutInflater.from(builder.context)
+            .inflate(R.layout.item_dialog,null)
+
+        val txt_message = view.findViewById<TextView>(R.id.txt_progress)
+        mMsg?.let {
+                txt_message.text = it
+        }
+        if(mTextColor != 0){
+            txt_message.setTextColor(mTextColor)
+        }
+        val container = view.findViewById<LinearLayout>(R.id.container)
+        if(mBackground == null){
+            container.background = ContextCompat.getDrawable(builder.context,
+                R.drawable.bg_container_progress)
+        } else {
+            container.background = mBackground
+        }
+
+        val progress = view.findViewById<ProgressBar>(R.id.progress)
+        if(mProgresssColor != 0){
+            DrawableCompat.setTint(progress.indeterminateDrawable,mProgresssColor)
+        }
+
+
+        val dialogbuilder = AlertDialog.Builder(builder.context)
+        dialogbuilder.setView(view)
+        dialogbuilder.setCancelable(false)
+        mDialog = dialogbuilder.create()
+        mDialog?.window?.setBackgroundDrawable(ColorDrawable(android.graphics.Color.TRANSPARENT))
+
+    }
+    fun show(){
+        mDialog?.show()
+    }
+    fun hide(){
+        mDialog?.hide()
+    }
+    fun setMessage(msg:String){
+        mMsg = msg
+    }
+}
---
+++ app/src/main/java/pk/farimarwat/simpleprogressbarexample/MainActivity.kt
@@ -0,0 +1,34 @@
+package pk.farimarwat.simpleprogressbarexample
+
+import android.content.Context
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.content.ContextCompat
+import pk.farimarwat.progressdialog.SimpleProgressDialog
+import pk.farimarwat.simpleprogressbarexample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var mContext:Context
+    val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    lateinit var mDialog:SimpleProgressDialog
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        mContext =  this
+        mDialog = SimpleProgressDialog.Builder(mContext)
+            .setMessage("Working... in progress")
+            .setBackgroundDrawable(
+                ContextCompat.getDrawable(mContext,R.drawable.background)
+            )
+            .build()
+        binding.button.setOnClickListener {
+            mDialog.show()
+        }
+        binding.button2.setOnClickListener {
+            mDialog.hide()
+        }
+    }
+}
